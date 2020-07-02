89e6ac9da1f77804b4d4b50a53e8266d125007c4
Jose Caraballo <jcaraballo74@hotmail.com>
2020-07-02 00:36:32 -0400

    Add AutoMapper

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
-using SocialMedia.Infrastructure.Repositories;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -11,10 +13,12 @@ namespace SocialMedia.Api.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
 
-        public PostController(IPostRepository postRepository)
+        public PostController(IPostRepository postRepository, IMapper mapper)
         {
             this._postRepository = postRepository;
+            this._mapper = mapper;
         }
 
         [HttpGet]
@@ -22,20 +26,53 @@ namespace SocialMedia.Api.Controllers
         {
             //var posts = new PostRepository().GetPosts();}
             var posts = await _postRepository.GetPosts();
-        return Ok(posts);
+            //AutoMapper
+            var postsDto = _mapper.Map<IEnumerable<PostDto>>(posts);
+
+            //var postsDto = posts.Select(x => new PostDto
+            //{
+            //    Postid = x.Postid,
+            //    Date = x.Date,
+            //    Image = x.Image,
+            //    UserId = x.UserId
+
+            //});
+            return Ok(postsDto);
         
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPosts(int id)
         {
             
-            var posts = await _postRepository.GetPosts(id);
-            return Ok(posts);
+            var post = await _postRepository.GetPosts(id);
+            var postsDto = _mapper.Map<PostDto>(post);
+            //var postsDto = new PostDto
+            //{
+            //    Postid = post.Postid,
+            //    Date = post.Date,
+            //    Image = post.Image,
+            //    UserId = post.UserId
+
+            //};
+            return Ok(postsDto);
 
         }
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            //Convertir Dto a entidad de Dominio
+            //Mapear
+            //var post = new Post
+            //{
+            //    Date = postDto.Date,
+            //    Image = postDto.Image,
+            //    UserId = postDto.UserId
+            //};
+
+
+            //AutoMapper
+            var post = _mapper.Map<Post>(postDto);
+
             await _postRepository.InsertPost(post);
             return Ok(post);
 
